67c84c75894d10849156f6f810e6c54d1222101c
Gene Hsu <gene@hsufarm.com>
2021-11-04 16:13:58 +0000

    Update rspec configuration

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,8 +6,13 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
+  config.order = :random
+
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+
+  # Enable focused specs
+  config.filter_run_when_matching :focus
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
